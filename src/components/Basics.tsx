bf56f2ecb8867476c5bcd40690250afa3a6923fd
= <angelino.n.lucas@gmail.com>
2021-05-27 16:36:36 -0300

    naboo-03

--- src/components/Basics.tsx
+++ src/components/Basics.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 const Basics = () => {
 
-    let nombre: string | number = 'Lucas';
-    nombre = 1234;
+    let nombre: string = 'Lucas';
+    const edad: number = 35;
+    let isActive :boolean = true;
+
+    const poderes: string[] = ['impak trueno', 'rayo'];
 
     return (
         <>
             <h3>Basics</h3>
-            { nombre }
+            { nombre }, { edad }, { isActive ? 'Active' : 'Not Active' }
+            <br />
+            { poderes.join(', ') }
         </>
     );
 };
