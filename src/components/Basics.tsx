6498cfa1950ad52c3dbd3760173ab3db6bb0fb68
= <angelino.n.lucas@gmail.com>
2021-05-27 16:24:41 -0300

    bazoo-2

--- src/components/Basics.tsx
+++ src/components/Basics.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Basics = () => {
+
+    let nombre: string | number = 'Lucas';
+    nombre = 1234;
+
     return (
         <>
             <h3>Basics</h3>
+            { nombre }
         </>
     );
 };
