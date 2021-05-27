d115c5d19b14636aec076976fd140f49db37095b
= <angelino.n.lucas@gmail.com>
2021-05-27 17:37:37 -0300

    naboo-07

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import './App.css';
 // import Basics from './components/Basics';
 // import Literals from './components/literals';
 // import Functiones from './components/Functiones';
-import { Contador } from './components/Contador';
+// import { Contador } from './components/Contador';
+// import { ContadorHook } from './components/ContadorHook';
+import { Login } from './components/Login';
 
 function App() {
   return (
-    <div className="mt-2">
+    <div className="m-5">
       <h1>Naboo</h1>
       <hr />
       {/* <Basics /> */}
       {/* <Literals /> */}
       {/* <Functiones /> */}
-      <Contador />
+      {/* <Contador /> */}
+      {/* <ContadorHook /> */}
+      <Login />
+      
     </div>
   );
 }
