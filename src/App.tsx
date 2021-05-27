128690d169368eabe7b296eee1615541653dcd3e
= <angelino.n.lucas@gmail.com>
2021-05-27 17:06:08 -0300

    naboo-05

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 // import Basics from './components/Basics';
-import Literals from './components/literals';
+// import Literals from './components/literals';
+import Functiones from './components/Functiones';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <h1>Naboo</h1>
       <hr />
       {/* <Basics /> */}
-      <Literals />
+      {/* <Literals /> */}
+      <Functiones />
     </div>
   );
 }
